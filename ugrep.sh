0799ffa8df2630af4136d1671348001df6de1670
dacort <d.lifehacker@gmail.com>
2008-07-16 19:30:54 -0700

    Updated $f variable to be more consistent/readable.

--- ugrep.sh
+++ ugrep.sh
@@ -2,6 +2,6 @@
 
 GREP_FOR=$1
 
-for f in `find . -type f | xargs -I {} file {} | grep UTF-16 | cut -f1 -d\:`
-	do iconv -f UTF-16 -t UTF-8 $f | grep -iH --label=$f ${GREP_FOR}
+for file in `find . -type f | xargs -I {} file {} | grep UTF-16 | cut -f1 -d\:`
+	do iconv -f UTF-16 -t UTF-8 ${file} | grep -iH --label=${file} ${GREP_FOR}
 done
