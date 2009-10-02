a6684dda815939d854184b78841a2551a2323d3a
dacort <d.lifehacker@gmail.com>
2009-10-02 10:25:20 -0400

    Now handles filenames with spaces in them

--- ugrep.sh
+++ ugrep.sh
@@ -2,6 +2,13 @@
 
 GREP_FOR=$1
 
+# Set file separator to deal with spaces
+O=$IFS
+IFS=$'\n'
+
 for file in `find . -type f | xargs -I {} file {} | grep UTF-16 | cut -f1 -d\:`
-	do iconv -f UTF-16 -t UTF-8 ${file} | grep -iH --label=${file} ${GREP_FOR}
+	do iconv -f UTF-16 -t UTF-8 "${file}" | grep -iH --label=${file} ${GREP_FOR}
 done
+
+# Reset file separator
+IFS=$O
